fa380f3c0c99ea0e1f0020f10ff600ebc72d161e
v.i.velikov00 <v.i.velikov00@gmail.com>
2024-05-26 12:32:30 +0000

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				olive: {
+					50: "#F3F7E8",
+					100: "#E9F1D5",
+					200: "#D3E3AB",
+					300: "#BDD481",
+					400: "#A7C657",
+					500: "#8BAB3A",
+					600: "#6F892F",
+					700: "#546723",
+					800: "#384517",
+					900: "#1C220C",
+					950: "#0C0F05",
+				},
+				primary: "#FCFCFA",
+				secondary: "#f5f5f8"
+			}
+		},
+	},
+	plugins: [],
+}
